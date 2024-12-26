d6e875f92598ad6c96c02749fbf2957d13624576
bahriah <bahriahkamardi@gmail.com>
2024-12-26 12:28:32 +0800

    menu navigate  user

---
+++ WAD_Project/menu.php
@@ -0,0 +1,7 @@
+<ul class="menu">
+  <li><a href="profile.php" class="menu-item <?php echo basename($_SERVER['PHP_SELF']) == 'profile.php' ? 'active' : ''; ?>">Profile</a></li>
+  <li><a href="services.html" class="menu-item <?php echo basename($_SERVER['PHP_SELF']) == 'services.html' ? 'active' : ''; ?>">Our Services</a></li>
+  <li><a href="booking_homepage.html" class="menu-item <?php echo basename($_SERVER['PHP_SELF']) == 'booking_homepage.html' ? 'active' : ''; ?>">My Appointment</a></li>
+  <li><a href="payment.html" class="menu-item <?php echo basename($_SERVER['PHP_SELF']) == 'payment.html' ? 'active' : ''; ?>">To Pay</a></li>
+  <li><a href="receipt.html" class="menu-item <?php echo basename($_SERVER['PHP_SELF']) == 'receipt.html' ? 'active' : ''; ?>">Transaction Details</a></li>
+</ul>
